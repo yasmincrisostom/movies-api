1de35ef0031e0949c4f1d6fb263a637c4826bbfc
Yasmin Crisostomo <crisostomyasmin@gmail.com>
2023-02-20 22:34:41 -0300

    movies controller

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -1,5 +1,25 @@
+require 'csv'
+
 class Api::V1::MoviesController < Api::V1::BaseController
   def index
-    @movies = params[:query].present? ? Movie.search(params[:query]) : Movie.all.order(:year)
+    limit = params[:limit] || 100
+    offset = params[:offset] || 0
+    if params[:query].present?
+      @movies = Movie.search(params[:query]).limit(limit).offset(offset)
+    else
+      @movies = Movie.all.order(:year).limit(limit).offset(offset)
+    end
+  end
+
+  def create
+    CSV.foreach(params[:file], headers: true).each do |row|
+      Movie.create({  id: SecureRandom.uuid,
+                      title: row['title'],
+                      genre: row['type'],
+                      year: row['release_year'],
+                      country: row['country'],
+                      published_at: row['date_added'],
+                      description: row['description'] })
+    end
   end
 end

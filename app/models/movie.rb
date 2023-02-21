19e14ffd3fb5506e9371ed615bce5db54b93e8d5
Yasmin Crisostomo <crisostomyasmin@gmail.com>
2023-02-20 22:35:13 -0300

    movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,8 @@
 class Movie < ApplicationRecord
   validates :id, uniqueness: true
+  validates :title, presence: true, uniqueness: true
 
   include PgSearch::Model
 
-  pg_search_scope :search, against: %i[genre year country]
+  pg_search_scope :search, against: %i[title genre year country]
 end
